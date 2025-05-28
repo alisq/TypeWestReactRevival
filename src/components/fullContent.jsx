de1002a2369d56278f1f8eb091159250f33bea41
alisq <ali.shamas.qadeer@gmail.com>
2025-05-28 11:41:03 -0400

    control panel moved to separate component!

--- src/components/fullContent.jsx
+++ src/components/fullContent.jsx
@@ -3,13 +3,14 @@ import React, { useEffect, useState } from 'react';
 import { sluggify } from '../functions';
 import Image from './image';
 import LoadFonts from './loadFonts';
+import ControlPanel from './controlPanel';
 
 
 
 function FullContent({ content, onClose, onNext, onPrev }) {
 
 
-    const [fontSize, setFontSize] = useState(36);
+ const [fontSize, setFontSize] = useState(36);
     const [leading, setLeading] = useState(12)
     const [activeOrientation, setActiveOrientation] = useState('left')
 
@@ -63,65 +64,15 @@ useEffect(()=>{
                 </div>
             </div>    
 
-
-                          
-                                
-                      <div className="control_panel">
-                        <div class="control_interior">
-
-                  <input
-                  className="slider"
-          type="range"
-          min="12"
-          max="72"
-          value={fontSize}
-          onChange={(e) => setFontSize(parseInt(e.target.value))}
-        />
-<div className="left">Size: <strong>{fontSize}px</strong></div>
-        <input 
-        className="slider"
-        type="range"
-        min="5" 
-        max="20"
-        value={leading}
-        onChange={(e)=>setLeading(e.target.value)}
-        />
-
-
-        <div className="left">Line Height: <strong>{leading/10}</strong></div>
-
-
- <div
-        className={`text_align left ${activeOrientation === 'left' ? 'active' : ''}`}
-        onClick={() => setActiveOrientation('left')}
-      >
-        
-            <div className='bar'></div>
-            <div className='bar'></div>
-            <div className='bar'></div>            
-        </div>
-
-        <div
-        className={`text_align center ${activeOrientation === 'center' ? 'active' : ''}`}
-        onClick={() => setActiveOrientation('center')}
-      >
-            <div className='bar'></div>
-            <div className='bar'></div>
-            <div className='bar'></div>            
-        </div>
-
-            <div
-        className={`text_align right ${activeOrientation === 'right' ? 'active' : ''}`}
-        onClick={() => setActiveOrientation('right')}
-      >
-            <div className='bar'></div>
-            <div className='bar'></div>
-            <div className='bar'></div>            
-        </div>
-        
-        </div>
-        
-        </div>      
+                                        
+                <ControlPanel
+                fontSize={fontSize}
+                setFontSize={setFontSize}
+                leading={leading}
+                setLeading={setLeading}
+                activeOrientation={activeOrientation}
+                setActiveOrientation={setActiveOrientation}
+                />
             
             <div className="tester_text">
 
